e4010c2924957b47343411fd041b86f2c2c00d74
Nanda Pratama Putra <60767596+nandappputra@users.noreply.github.com>
2022-12-21 16:00:38 +0700

    Add exercise 3.19

--- index.js
+++ index.js
@@ -132,6 +132,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformed id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).send({ error: error.message });
   }
 
   next(error);
